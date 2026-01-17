effa37307abe36d3dfcbe2ee77b3b3b36f2925b3
andergi <agibarguen@gmail.com>
2026-01-17 13:34:51 +0100

    test(backoffice): added backoffice health check controller test class.
    At the moment of this commit, the class is only testing one happy path. It only checks for a 200 status code when a GET request to /health-check occurs.

--- apps/test/com/andergi/apps/backoffice/controllers/healthcheck/HealthCheckGetControllerShould.java
+++ apps/test/com/andergi/apps/backoffice/controllers/healthcheck/HealthCheckGetControllerShould.java
@@ -1,4 +1,11 @@
-import static org.junit.jupiter.api.Assertions.*;
-class HealthCheckGetControllerTest {
-  
+package com.andergi.apps.backoffice.controllers.healthcheck;
+
+import com.andergi.apps.RequestTestCase;
+import org.junit.jupiter.api.Test;
+
+final class HealthCheckGetControllerShould extends RequestTestCase {
+    @Test
+    void check_the_app_is_working_ok() throws Exception{
+        assertResponseWithNoBody("/health-check", 200);
+    }
 }
