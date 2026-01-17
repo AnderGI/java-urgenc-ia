4ffa52896a8032bb3b37d652bf94373afae44e79
andergi <agibarguen@gmail.com>
2026-01-17 20:13:21 +0100

    config(feature/backoffice/register-product): added springs service annotation for the full ATDD lifecycle regarding the product registrar

--- apps/main/com/andergi/apps/backoffice/controllers/product/register_product/ProductPutController.java
+++ apps/main/com/andergi/apps/backoffice/controllers/product/register_product/ProductPutController.java
@@ -1,6 +1,7 @@
 package com.andergi.apps.backoffice.controllers.product.register_product;
 
 
+import com.andergi.backoffice.product.application.ProductRegisterer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 final public class ProductPutController {
 
+    private final ProductRegisterer registerer;
+
+    public ProductPutController(ProductRegisterer registerer) {
+        this.registerer = registerer;
+    }
+
     @PutMapping(path = "/products/{id}")
     public ResponseEntity run(@PathVariable String id, @RequestBody ProductPutRequest productPutRequest) {
+        registerer.register(id, productPutRequest.name());
         return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 }
--- src/backoffice/main/com/andergi/backoffice/product/application/ProductRegisterer.java
+++ src/backoffice/main/com/andergi/backoffice/product/application/ProductRegisterer.java
@@ -2,7 +2,10 @@ package com.andergi.backoffice.product.application;
 
 import com.andergi.backoffice.product.domain.Product;
 import com.andergi.backoffice.product.domain.ProductRepository;
+import org.springframework.stereotype.Service;
 
+// TODO change to a custom service annotation to contain vendor logic in one place only
+@Service
 final public class ProductRegisterer {
 
     private final ProductRepository repository;
